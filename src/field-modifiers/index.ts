8c39740752c395846539fd96d37e205f197595b2
Chinedu Daniel <chinedudaniel7@gmail.com>
2021-03-29 22:34:38 +0100

    Fix for failing build

--- src/field-modifiers/index.ts
+++ src/field-modifiers/index.ts
@@ -3,11 +3,13 @@ import { primary } from './primary';
 import { unique } from './unique';
 import { fieldDefault } from './default';
 import { list } from './list';
+import { updatedAt } from './updatedAt';
 
 export const fieldModifierFns = {
   optional,
   primary,
   unique,
   default: fieldDefault,
-  list
+  list,
+  updatedAt,
 };

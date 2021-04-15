9307b1067a5b39b975da53f2d0ce3471bc44019e
Chinedu Daniel <chinedudaniel7@gmail.com>
2021-04-15 10:34:46 +0200

    Fix for lint issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// import * as Defaults from './attribute-fns';
+import * as Defaults from './attribute-fns';
 
-export * as Defaults from './attribute-fns';
-export { enumType } from './enum'
+export { Defaults };
+export { enumType } from './enum';
 export { model } from './model';
